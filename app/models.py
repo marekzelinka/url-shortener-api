07e50a4bf4275631d62ddec7db740217ce56fc2a
Marek Zelinka <mzelinka17@gmail.com>
2026-02-12 15:13:00 +0100

    add user document

--- app/models.py
+++ app/models.py
@@ -3,9 +3,8 @@ from enum import Enum
 from functools import partial
 from typing import Annotated, TypeVar
 
-from beanie import Document, Indexed
-from beanie.odm.enums import SortDirection
-from pydantic import AnyUrl, BaseModel, ConfigDict, Field, field_validator
+from beanie import Document, Indexed, PydanticObjectId, SortDirection
+from pydantic import AnyUrl, BaseModel, ConfigDict, EmailStr, Field, field_validator
 from pydantic.generics import GenericModel
 
 SchemaType = TypeVar("SchemaType", bound=BaseModel)
@@ -49,18 +48,38 @@ class SortingParams(BaseModel):
     order: SortOrder = SortOrder.ASC
 
 
-class ShortUrlCreate(BaseModel):
-    url: AnyUrl
-    slug: Annotated[str | None, Field(min_length=1, max_length=64)] = None
-    expiration_days: Annotated[float | None, Field(ge=0.0)] = None
+class User(Document):
+    username: Annotated[str, Indexed(unique=True)]
+    email: Annotated[EmailStr, Indexed(unique=True)]
+    hashed_password: str
+    is_active: bool = True
+    is_superuser: bool = False
+    created_at: Annotated[
+        datetime, Field(default_factory=partial(datetime.now, tz=UTC))
+    ]
 
-    @field_validator("slug", mode="after")
-    @classmethod
-    def normalize_slug(cls, slug: str | None) -> str | None:
-        """Transforms existing slug by lowercasing and replacing spaces with dashes."""
-        if slug is not None:
-            slug = slug.strip().lower().replace(" ", "-")
-        return slug
+
+class UserBase(BaseModel):
+    username: Annotated[str, Field(min_length=3, max_length=64)]
+    email: EmailStr
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdate(BaseModel):
+    username: str | None = None
+    email: EmailStr | None = None
+    is_active: bool | None = None
+    is_superuser: bool | None = None
+    password: str | None = None
+
+
+class UserPrivate(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    created_at: datetime
 
 
 class ShortUrl(Document):
@@ -77,6 +96,21 @@ class ShortUrl(Document):
     expires_at: Annotated[datetime | None, Indexed(expireAfterSeconds=0)] = None
     last_visit_at: datetime | None = None
     slug: Annotated[str | None, Indexed(unique=True)] = None
+    user_id: PydanticObjectId
+
+
+class ShortUrlCreate(BaseModel):
+    url: AnyUrl
+    slug: Annotated[str | None, Field(min_length=1, max_length=64)] = None
+    expiration_days: Annotated[float | None, Field(ge=0.0)] = None
+
+    @field_validator("slug", mode="after")
+    @classmethod
+    def normalize_slug(cls, slug: str | None) -> str | None:
+        """Transforms existing slug by lowercasing and replacing spaces with dashes."""
+        if slug is not None:
+            slug = slug.strip().lower().replace(" ", "-")
+        return slug
 
 
 class ShortUrlPublic(BaseModel):
